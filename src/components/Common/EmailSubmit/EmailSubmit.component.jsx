9320a6c6ee367c798b557dd1538be9523fb24a21
Jordan Russel <rudzinski.a@gmail.com>
2021-05-24 23:18:17 +0200

    windowresize event

--- src/components/Common/EmailSubmit/EmailSubmit.component.jsx
+++ src/components/Common/EmailSubmit/EmailSubmit.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Button from '../../Common/Button/Button.component';
 import FooterTitle from '../../Common/Layout/Footer/FooterTitle/FooterTitle.component';
@@ -6,16 +6,23 @@ import { StyledInput, StyledForm, StyledLabel, StyledEmailSubmitWrapper } from '
 
 import FOOTER_DATA from '../../../data/footer.data';
 
-const EmailSubmit = () => (
+const EmailSubmit = () => {
+    const [windowWidth, setWindowWidth] = useState(0)
+
+    useEffect(() => {
+        setWindowWidth(window.innerWidth)
+    }, [])
+
+    return (
     <StyledEmailSubmitWrapper>
         <FooterTitle>{FOOTER_DATA.title}</FooterTitle>
         <StyledForm action="." method="post">
             <StyledLabel>email</StyledLabel>
             <StyledInput placeholder="Enter your email" type="email" name="email" id="email"/>
-            {window.innerWidth > 500 ? <Button type="submit" width={113} height={34} font={18} radius={4}>Get started</Button> : ''}        
+            {windowWidth > 500 ? <Button type="submit" width={113} height={34} font={18} radius={4}>Get started</Button> : ''}        
         </StyledForm>
-        {window.innerWidth > 500 ? '' : <Button type="submit" height={61}>Get started</Button>}        
+        {windowWidth > 500 ? '' : <Button type="submit" height={61}>Get started</Button>}        
     </StyledEmailSubmitWrapper>
-)
+)}
 
 export default EmailSubmit;
