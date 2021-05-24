e075041595a103b14ce04b076761be1aead5bc60
Jordan Russel <rudzinski.a@gmail.com>
2021-05-24 23:20:51 +0200

    url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  
   siteMetadata: {
     title: "Meldi",
+    siteUrl: "https://alanrudzinski.github.io/meldi-daftcode/"
   },
   plugins: [
     "gatsby-plugin-styled-components",
